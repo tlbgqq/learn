ee79dfa51422fba5be7ba6a2373e8b007603d723
tlbgqq <157470556@qq.com>
2026-04-29 20:02:08 +0800

    token获取

--- study-agent/frontend/src/api/index.js
+++ study-agent/frontend/src/api/index.js
@@ -10,7 +10,7 @@ const api = axios.create({
 
 api.interceptors.request.use(
   config => {
-    const token = localStorage.getItem('token')
+    const token = localStorage.getItem('admin_token') || localStorage.getItem('token')
     if (token) {
       config.headers.Authorization = `Bearer ${token}`
     }
